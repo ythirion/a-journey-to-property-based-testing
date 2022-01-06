e17df8f4883e8dc5208f62cf001833d3cec8415c
Yoan Thirion <yoan.thirion@gmail.com>
2022-01-06 16:19:12 +0100

    Fix scala version

--- scala/src/test/scala/bank/solution/WithdrawExampleTests.scala
+++ scala/src/test/scala/bank/solution/WithdrawExampleTests.scala
@@ -4,6 +4,8 @@ import bank.solution.WithdrawExampleTests._
 import bank.{Account, AccountService, Amount, Withdraw}
 import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers.{be, contain, have, startWith}
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import org.scalatest.prop.TableDrivenPropertyChecks
 import org.scalatest.prop.Tables.Table
 import org.scalatestplus.scalacheck.Checkers
@@ -35,11 +37,11 @@ class WithdrawExampleTests
 
         expectedResult match {
           case Left(expectedErrorMessage) =>
-            debitedAccount.left.value.startsWith(expectedErrorMessage)
+            debitedAccount.left.value should startWith(expectedErrorMessage)
           case Right(expectedBalance) =>
-            debitedAccount.value.balance == expectedBalance &&
-              debitedAccount.value.withdraws.contains(command) &&
-              debitedAccount.value.withdraws.length == 1
+            debitedAccount.value.balance should be(expectedBalance)
+            debitedAccount.value.withdraws should contain(command)
+            debitedAccount.value.withdraws should have length 1
         }
     }
   }
@@ -59,8 +61,8 @@ object WithdrawExampleTests {
       (0, true, 500, 50d, Right(-50)),
       (1, false, 1200, 1199.99, Left("Insufficient balance to withdraw")),
       (0, false, 500, 50d, Left("Insufficient balance to withdraw")),
-      (10_000, true, 1200, 1200.0001, Left("Insufficient balance to withdraw")),
-      (0, false, 500, 500d, Left("Insufficient balance to withdraw"))
+      (10_000, true, 1200, 1200.0001, Left("Amount exceeding your limit of")),
+      (0, false, 500, 500d, Left("Amount exceeding your limit of"))
     )
 
   private def toWithdraw(amount: Double): Withdraw = {
