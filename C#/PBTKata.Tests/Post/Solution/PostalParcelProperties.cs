be0b1d8a9fe4f880fc5eab735b855b5b494b93a6
Yoan Thirion <yoan.thirion@gmail.com>
2022-01-06 11:19:18 +0100

    add step by step for Postal Properties

--- C#/PBTKata.Tests/Post/Solution/PostalParcelProperties.cs
+++ C#/PBTKata.Tests/Post/Solution/PostalParcelProperties.cs
@@ -10,22 +10,22 @@ using static PBTKata.Post.PostalParcelService;
 namespace PBTKata.Tests.Post.Solution
 {
     public class PostalParcelProperties
-    {
+    {       
         [Fact]
         public void MaxDeliveryCostWhenWeightGreaterThanMaxWeight()
-            => Prop.ForAll(Arb.From<Double>().Filter(x => x > MaxWeight),
+            => Prop.ForAll(Arb.Default.Float().Filter(x => x > MaxWeight),
                     weight => CalculateCost(weight) == MaxDeliveryCost)
                     .QuickCheckThrowOnFailure();
 
         [Fact]
         public void MinDeliveryCostWhenWeightLowerOrEqualsMaxWeight()
-            => Prop.ForAll(Arb.From<Double>().Filter(x => x > 0 && x <= MaxWeight),
+            => Prop.ForAll(Arb.Default.Float().Filter(x => x > 0 && x <= MaxWeight),
                     weight => CalculateCost(weight) == MinDeliveryCost)
                     .QuickCheckThrowOnFailure();
 
         [Fact]
         public void NoneWhenWeightLowerOrEquals0()
-            => Prop.ForAll(Arb.From<Double>().Filter(x => x <= 0),
+            => Prop.ForAll(Arb.Default.Float().Filter(x => x <= 0),
                     weight => CalculateCost(weight) == None)
                     .QuickCheckThrowOnFailure();
 
