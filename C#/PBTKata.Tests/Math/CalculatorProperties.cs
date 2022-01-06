71b38b95faff6f568b8b578d89855b890ee7c0ec
Yoan Thirion <yoan.thirion@gmail.com>
2022-01-06 08:47:31 +0100

    Fix previous commit (shitty UI)

---
+++ C#/PBTKata.Tests/Math/CalculatorProperties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
+using static PBTKata.Math.Calculator;
+
+namespace PBTKata.Tests.Math
+{
+    public class CalculatorProperties
+    {
+        [Property]
+        public Property Commutativity(int x, int y)
+            => (Add(x, y) == Add(y, x)).ToProperty();
+
+        [Property]
+        public Property Associativity(int x)
+            => (Add(Add(x, 1), 1) == Add(x, 2)).ToProperty();
+
+        [Property]
+        public Property Identity(int x)
+            => (Add(x, 0) == x).ToProperty();
+    }
+}
---
+++ C#/PBTKata/Post/PostalParcel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LanguageExt;
+using static LanguageExt.Option<PBTKata.Post.PostalParcel>;
+
+namespace PBTKata.Post
+{
+    public record PostalParcel
+    {
+        public double Weight { get; private init; }
+        public static Option<PostalParcel> From(double weight) =>
+            weight > 0 ? new PostalParcel { Weight = weight } : None;
+    }
+
+    public static class PostalParcelService
+    {
+        public const double MaxWeight = 20;
+        public const double MinDeliveryCost = 1.99;
+        public const double MaxDeliveryCost = 4.99;
+
+        public static Option<double> CalculateDeliveryCosts(Option<PostalParcel> postalParcel) =>
+            postalParcel.Map(p => p.Weight > MaxWeight ? MaxDeliveryCost : MinDeliveryCost);
+    }
+}
